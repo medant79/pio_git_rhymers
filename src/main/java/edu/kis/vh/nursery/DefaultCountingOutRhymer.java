2f18ddc3f24eb5c386927f9c9b3593a29a565714
szymonmuszynski1 <mouchehehe00@gmail.com>
2024-05-19 18:38:58 +0200

    6.1. Dodanie brakujących deklaracji stałych i zmiana metody nazewnictwa

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,15 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int errorValue = -1;
-    public static final int size = 12;
-    private int[] numbers = new int[size];
+    public static final int DEFAULT_RETURN = -1;
 
-    public int total = -1;
+    public static final int MAX_SIZE = 12;
+    public static final int ARRAY_FULL = MAX_SIZE - 1;
+    public static final int TOTAL_DEFAULT_VALUE = -1;
+    public static final int CALL_CHECK_VALUE = -1;
+    private int[] numbers = new int[MAX_SIZE];
+
+    public int total = TOTAL_DEFAULT_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -14,22 +18,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == errorValue;
+        return total == CALL_CHECK_VALUE;
     }
         
     public boolean isFull() {
-        return total == 11;
+        return total == ARRAY_FULL;
     }
         
     protected int peekaboo() {
         if (callCheck())
-            return errorValue;
+            return DEFAULT_RETURN;
         return numbers[total];
     }
             
     public int countOut() {
         if (callCheck())
-            return errorValue;
+            return DEFAULT_RETURN;
         return numbers[total--];
     }
 
